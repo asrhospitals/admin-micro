206f77c5b6f7ce96357d489e316a9e1669ba90d8
Aanandkumar <anandkumaraadhi@gmail.com>
2025-09-08 20:24:37 +0530

    Modified data

--- validations/colorValidation.js
+++ validations/colorValidation.js
@@ -1,17 +1,18 @@
 const Joi = require("joi");
 
+
 const colorSchema = Joi.object({
   colorstatus: Joi.string().required(),
-  colorcodes: Joi.object({
+  colorcode: Joi.object({
     hex: Joi.string().pattern(/^#([0-9A-Fa-f]{3}){1,2}$/).optional(),
     rgb: Joi.string().pattern(/^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/).optional(),
     hsl: Joi.string().pattern(/^hsl\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%\)$/).optional(),
-  })
-    .min(1)
-    .required(),
+    name: Joi.string().optional(), // ✅ allow color name
+  }).min(1).required(),
   createdAt: Joi.date().optional(),   // <-- allow frontend to send it
-  // updatedAt: Joi.date().optional(),   // optional if needed
+
 });
+
 
 
 module.exports = { colorSchema };
