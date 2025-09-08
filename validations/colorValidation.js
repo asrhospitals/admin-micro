9b27722878c02fd22b6a2c4b1f8d27a63ac91155
Aanandkumar <anandkumaraadhi@gmail.com>
2025-09-08 19:45:16 +0530

    Modified Color Validations

--- validations/colorValidation.js
+++ validations/colorValidation.js
@@ -4,15 +4,12 @@ const colorSchema = Joi.object({
   colorstatus: Joi.string().required(),
   colorcodes: Joi.object({
     hex: Joi.string().pattern(/^#([0-9A-Fa-f]{3}){1,2}$/).optional(),
-    rgb: Joi.string()
-      .pattern(/^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/)
-      .optional(),
-    hsl: Joi.string()
-      .pattern(/^hsl\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%\)$/)
-      .optional(),
+    rgb: Joi.string().pattern(/^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/).optional(),
+    hsl: Joi.string().pattern(/^hsl\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%\)$/).optional(),
   })
     .min(1)
     .required(),
 });
+
 
 module.exports = { colorSchema };
